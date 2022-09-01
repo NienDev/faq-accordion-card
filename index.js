0e4ff55f4cb41a7621560e5398878d5193dbd7dd
NienDev <niewebdev@gmail.com>
2022-09-01 21:11:22 +0700

    finish

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ function makeActive(e){
   const downArrow = e.target.parentElement.querySelector('.bx-chevron-down');
   const upArrow = e.target.parentElement.querySelector('.bx-chevron-up');
   const answer = e.target.parentElement.parentElement.querySelector('.answer')
+  const question = e.target.parentElement.querySelector('.question');
   if (downArrow.classList.contains('hidden')){
     downArrow.classList.remove('hidden')
     upArrow.classList.add('hidden')
     answer.classList.add('hidden')
+    question.classList.remove('font-bold')
   }else{
     downArrow.classList.add('hidden')
     upArrow.classList.remove('hidden')
     answer.classList.remove('hidden')
+    question.classList.add('font-bold')
   }
 }
 
